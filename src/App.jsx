f0d5895ecb3ff01a6b207b9744371a8a53160ac6
cadelme <cdelgado.es@gmail.com>
2024-04-18 13:49:16 +0200

    clase4

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,37 @@
-import { ThemeProvider } from "@mui/material"
-import Navbar from "./components/layout/navbar/NavbBar"
-import { themeStore } from "./components/themeConfig"
-import ItemListContainer from "./components/pages/itemListContainer/ItemListContainer"
+import { useState } from "react";
+import { Counter } from "./components/common/counter/Counter";
+import { Footer } from "./components/layout/footer/Footer";
+import { Navbar } from "./components/layout/navbar/Navbar";
+import ItemListContainer from "./components/pages/itemListContainer/ItemListContainer";
 
 function App() {
+  // name  ---> pepe
+  // name ---> juan
+
+  const [name, setName] = useState("pepe");
+
+  // const cambiarNombre = (nuevoNombre) => {
+  //   setName(nuevoNombre);
+  // };
+  
+
   return (
-    <ThemeProvider theme={themeStore}>
-      <div>
-        <Navbar />
-        <ItemListContainer greeting={"Bienvenidos a mi Tienda"} />
-      </div>
-    </ThemeProvider>
-  )
+    <div>
+      <Navbar />
+      <ItemListContainer />
+      <Footer />
+
+      <Counter />
+
+      <h2>{name}</h2>
+      {/* <button onClick={cambiarNombre}>Cambiar nombre</button> */}
+
+      {/* <button onClick={cambiarNombre("fulanito")}>Cambiar nombre</button> */}
+      {/* <button onClick={()=> cambiarNombre("fulanito")}>Cambiar nombre</button> */}
+      <button onClick={()=>setName("juan")}>Cambiar</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
